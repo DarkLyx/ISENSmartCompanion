b2cec59d3292930d027a1ce8b04ec6f2fcc16fff
Lucas <lucas.gumuchian16@gmail.com>
2025-03-06 22:41:13 +0100

    fix add log

--- app/src/main/java/fr/isen/lucas/isensmartcompanion/MainActivity.kt
+++ app/src/main/java/fr/isen/lucas/isensmartcompanion/MainActivity.kt
@@ -81,5 +81,29 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    // Override lifecycle methods...
+    override fun onStart() {
+        super.onStart()
+        Log.d("MainActivity", "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("MainActivity", "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("MainActivity", "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("MainActivity", "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MainActivity", "onDestroy called")
+    }
 }
+
